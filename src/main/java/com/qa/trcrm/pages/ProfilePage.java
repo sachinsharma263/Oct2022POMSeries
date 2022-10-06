35106defa342aacc275161177010cb1254bcffc5
Sachin Sharma <68374560+sachinsharma263@users.noreply.github.com>
2022-10-06 14:44:53 +0530

    Create ProfilePage.java

---
+++ src/main/java/com/qa/trcrm/pages/ProfilePage.java
@@ -0,0 +1,17 @@
+package com.qa.trcrm.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.trcrm.base.BasePage;
+
+public class ProfilePage extends BasePage {
+
+	WebDriver driver;
+
+	public ProfilePage(WebDriver driver) {
+		this.driver = driver;
+	}
+ public void remote() {
+		System.out.println("remote-changes");
+	}
+}
